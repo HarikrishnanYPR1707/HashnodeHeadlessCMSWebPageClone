926054f697ee28470ad913a95b77c4febbbb2ad1
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-16 21:33:34 +0530

    Added hashnode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      hashnodeOrange: "#fb923c",
+      hashnodeYellow: "#facc15",
+      hashnodePink: "#ec4899",
+      hashnodeLightPurple: "#d8b4fe",
+      hashnodeDarkPurple: "#9333ea",
+      hashnodeLightGreen: "#86efac",
+      hashnodeDarkGreen: "#16a34a",
+      hashnodeLightBlue: "#3b82f6",
+      hashnodeDarkBlue: "#010410",
+    },
   },
   plugins: [],
 };
