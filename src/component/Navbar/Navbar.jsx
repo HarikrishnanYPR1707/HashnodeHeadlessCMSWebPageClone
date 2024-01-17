bba53102f0102b27db2fab2e32f6635d0f19d57b
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-17 23:16:17 +0530

    Added Changes

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="border bg-hashnodeDarkBlue border-red-500 w-full">
+      <div className="h-20 border w-20"></div>
+    </div>
+  );
 };
 
 export default Navbar;
