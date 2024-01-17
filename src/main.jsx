89ad92c6364d6d27930cba0474fb9f8c2d4538af
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-17 23:09:15 +0530

    Added Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
 
 const routes = createBrowserRouter([
   {
